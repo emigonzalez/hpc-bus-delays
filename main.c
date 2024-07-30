7f8b6d97566947dfae9b16fe11e28ad83ffb5b20
Emiliano Gonzalez <gonzalez.emiliano@fing.edu.uy>
2024-07-29 23:23:40 -0300

    update data_grouping and main to handle new hash_map

--- main.c
+++ main.c
@@ -110,184 +110,13 @@ void handle_signal(int signal) {
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <semaphore.h>
-
-#define SHM_NAME "/vfd_map_shm"
-#define SHM_SIZE 1024*1024
 
 HashMap *vfd_map = NULL;
 
-void copy_hash_map_to_shared_memory(HashMap *vfd_map, void *shared_memory) {
-    // Puntero para recorrer la memoria compartida
-    void *current_position = shared_memory;
-    // Copiar HashMap
-    HashMap *shm_hash_map = (HashMap *)current_position;
-    memcpy(shm_hash_map, vfd_map, sizeof(HashMap));
-    current_position += sizeof(HashMap);
-
-    // Copiar campos_capturas
-    char **src_campos_capturas = vfd_map->campos_capturas;
-    char **dst_campos_capturas = (char **)current_position;
-    size_t num_campos_capturas = 15; //vfd_map->size; // Asegúrate de usar el número correcto
-    current_position += num_campos_capturas * sizeof(char *);
-
-    for (size_t i = 0; i < num_campos_capturas; ++i) {
-        size_t str_len = strlen(src_campos_capturas[i]) + 1;
-        dst_campos_capturas[i] = (char *)current_position;
-        memcpy(dst_campos_capturas[i], src_campos_capturas[i], str_len);
-        current_position += str_len;
-    }
-    
-    shm_hash_map->campos_capturas = (char **)shared_memory + sizeof(HashMap);
-
-    
-    // Copiar campos_horarios de forma similar
-    char **src_campos_horarios = vfd_map->campos_horarios;
-    char **dst_campos_horarios = (char **)current_position;
-    size_t num_campos_horarios = 9; //vfd_map->size; // Ajusta según sea necesario
-    current_position += num_campos_horarios * sizeof(char *);
-
-    for (size_t i = 0; i < num_campos_horarios; ++i) {
-        size_t str_len = strlen(src_campos_horarios[i]) + 1;
-        dst_campos_horarios[i] = (char *)current_position;
-        memcpy(dst_campos_horarios[i], src_campos_horarios[i], str_len);
-        current_position += str_len;
-    }
-    
-    shm_hash_map->campos_horarios = (char **)shared_memory + sizeof(HashMap) + num_campos_capturas * sizeof(char *);
-
-    // Copiar las entradas del HashMap
-    Entry **src_entries = vfd_map->buckets;
-    Entry **dst_entries = (Entry **)current_position;
-    current_position += vfd_map->size * sizeof(Entry *);
-
-    for (size_t i = 0; i < vfd_map->size; ++i) {
-        if (src_entries[i] != NULL) {
-            size_t entry_size = sizeof(Entry) + src_entries[i]->vft_count * sizeof(VFT *);
-            dst_entries[i] = (Entry *)current_position;
-            memcpy(dst_entries[i], src_entries[i], entry_size);
-            current_position += entry_size;
-
-            // Copiar VFTs y VFDs si están presentes
-            VFT **src_vfts = src_entries[i]->vfts;
-            VFT **dst_vfts = dst_entries[i]->vfts;
-            for (size_t j = 0; j < src_entries[i]->vft_count; ++j) {
-                size_t vft_size = sizeof(VFT);
-                dst_vfts[j] = (VFT *)current_position;
-                memcpy(dst_vfts[j], src_vfts[j], vft_size);
-                current_position += vft_size;
-            }
-
-            VFD **src_vfds = src_entries[i]->vfds;
-            VFD **dst_vfds = dst_entries[i]->vfds;
-            for (size_t j = 0; j < src_entries[i]->vfd_count; ++j) {
-                size_t vfd_size = sizeof(VFD);
-                dst_vfds[j] = (VFD *)current_position;
-                memcpy(dst_vfds[j], src_vfds[j], vfd_size);
-                current_position += vfd_size;
-            }
-        }
-    }
-}
-
-
-size_t calculate_hash_map_size(HashMap *map) {
-    size_t size = sizeof(HashMap);
-
-    // Suponiendo que map->buckets es un array de punteros a Entry
-    size += sizeof(Entry*) * map->size;
-
-    // Calcular tamaño de campos_capturas
-    if (map->campos_capturas) {
-        size += sizeof(char*) * map->count;
-        for (size_t i = 0; i < map->count; ++i) {
-            size += strlen(map->campos_capturas[i]) + 1; // +1 para el carácter null
-        }
-    }
-
-    // Calcular tamaño de campos_horarios
-    if (map->campos_horarios) {
-        size += sizeof(char*) * map->count;
-        for (size_t i = 0; i < map->count; ++i) {
-            size += strlen(map->campos_horarios[i]) + 1; // +1 para el carácter null
-        }
-    }
-
-    // Añadir tamaño de cada Entry
-    for (size_t i = 0; i < map->size; ++i) {
-        Entry *entry = map->buckets[i];
-        while (entry) {
-            // Añadir tamaño de cada Entry
-            size += sizeof(Entry);
-
-            // Añadir tamaño de VFT* y VFD* en Entry
-            size += sizeof(VFT*) * entry->vft_capacity;
-            for (size_t j = 0; j < entry->vft_count; ++j) {
-                size += sizeof(VFT);
-                // Añadir tamaño de los campos en VFT
-                size += strlen(entry->vfts[j]->cod_variante) + 1;
-                size += strlen(entry->vfts[j]->frecuencia) + 1;
-                // ... Hacer lo mismo para otros campos en VFT
-            }
-
-            size += sizeof(VFD*) * entry->vfd_capacity;
-            for (size_t j = 0; j < entry->vfd_count; ++j) {
-                size += sizeof(VFD);
-                // Añadir tamaño de los campos en VFD
-                size += strlen(entry->vfds[j]->id) + 1;
-                size += strlen(entry->vfds[j]->codigoEmpresa) + 1;
-                // ... Hacer lo mismo para otros campos en VFD
-            }
-
-            entry = entry->next;
-        }
-    }
-
-    return size;
-}
-
-
 void run_python_script(const char *script_name) {
-    // Open the shared memory object
-    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        perror("shm_open");
-        exit(1);
-    }
-
-    // Set the size of the shared memory object
-    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
-        perror("ftruncate");
-        exit(1);
-    }
-
-    // Map the shared memory object
-    void* shm_base = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (shm_base == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }
-
-    // Copy vfd_map to the shared memory
-    // memcpy(shm_base, vfd_map, sizeof(HashMap));
-    deep_copy_hashmap_to_shared_memory(vfd_map,shm_base);
-
-    // Assume we have a pointer to the end of the shared memory
-    void *shm_end = shm_base + SHM_SIZE;  // Adjust based on your actual end pointer logic
-
-    // Calculate the size to synchronize
-    size_t sync_size = shm_end - shm_base;
-
-    // Synchronize the shared memory
-    if (msync(shm_base, sync_size, MS_SYNC) == -1) {
-        perror("msync");
-        exit(1);
-    }
-
     // Call the Python script
     char command[256];
     snprintf(command, sizeof(command), "python3 access_vfd_map.py");
@@ -309,16 +138,6 @@ void run_python_script(const char *script_name) {
     if (status == -1) {
         perror("pclose");
     }
-
-    // The shared memory now contains a deep copy of the HashMap
-    // printf("Deep copy to shared memory completed successfully\n");
-
-    // Cleanup
-    munmap(shm_base, SHM_SIZE);
-    close(shm_fd);
-
-    // Unlink the shared memory object and semaphore only after Python script finishes
-    shm_unlink(SHM_NAME);
 }
 
 int main() {
@@ -344,26 +163,18 @@ int main() {
     // Example: Print grouped data
     print_hash_map(vfd_map);
 
-    char** campos = (get_campos_capturas(vfd_map));
-    for (size_t i = 0; i < 15; ++i) {
-        char*  campo = campos[i];
-        printf("%s", campo);
-        printf("\n");
-        
-    }
-
     printf("HashMap size: %zu\n", vfd_map->size);
     printf("HashMap count: %zu\n", vfd_map->count);
 
     // Path to the Python script
-    const char *script_name = "access_vfd_map.py";
+    // const char *script_name = "access_vfd_map.py";
 
-    printf("CALLING PYTHON SCRIPT %s \n", script_name);
+    // printf("CALLING PYTHON SCRIPT %s \n", script_name);
 
     // Call the function to run the Python script with the hash map pointer
-    run_python_script(script_name);
+    // run_python_script(script_name);
 
-    printf("END PYTHON :) \n");    
+    // printf("END PYTHON :) \n");    
 
     // Free the hash map
     free_hash_map(vft_map);
--- src/data_grouping.c
+++ src/data_grouping.c
@@ -9,12 +9,6 @@
 #include "data_grouping.h"
 #include "date_to_day_type.h"
 
-/** TODO
- * Al agrupar por vfd, conseguir vft.
- * Almacenar los descartes. Descartar vfds sin vft.
- * retornar lista de vfds (solo nombres)
- */
-
 int is_valid_departure_time(const char* frecuencia) {
     int len = strlen(frecuencia);
     if (len < 4 || len > 6) return 0; // Must be 4 to 6 characters long to include the trailing zero
@@ -88,58 +82,26 @@ void ajustar_fecha(const char* fecha, const char* frecuencia, char* resultado) {
     }
 }
 
-void add_vft_to_map(HashMap *map, const char *key, VFT *vft) {
-    if (!hash_map_insert_vft(map, key, vft)) {
+Entry* add_vft_to_map(HashMap *map, const char *key, const char *row) {
+    Entry* entry = hash_map_insert_vft(map, key, row);
+    if (!entry) {
         fprintf(stderr, "Error: failed to add VFT to map\n");
         exit(EXIT_FAILURE);
     }
+    return entry;
 }
 
-Entry* add_vfd_to_map(HashMap *map, const char *key, VFD *vfd) {
-    Entry* vfd_entry = hash_map_insert_vfd(map, key, vfd);
+Entry* add_vfd_to_map(HashMap *map, const char *key, const char *row) {
+    if (key == NULL || row == NULL) return NULL;
+
+    Entry* vfd_entry = hash_map_insert_vfd(map, key, row);
     if (!vfd_entry) {
         fprintf(stderr, "Error: failed to add VFD to map\n");
     }
     return vfd_entry;
 }
 
-VFT* row_to_vft(char* line) {
-    char* tipo_dia = strtok(line, ";");
-    char* variante = strtok(NULL, ";");
-    char* frecuencia = strtok(NULL, ";");
-    char* cod_ubic_parada = strtok(NULL, ";");
-    char* ordinal = strtok(NULL, ";");
-    char* hora = strtok(NULL, ";");
-    char* dia_anterior = strtok(NULL, ";");
-    // char* latitud = strtok(NULL, ";");
-    // char* longitud = strtok(NULL, ";");
-
-    if (tipo_dia == NULL || variante == NULL || frecuencia == NULL || dia_anterior == NULL) {
-        fprintf(stderr, "Error: Missing data from row to VFT in line: %s", line);
-        return NULL;
-    }
-
-    // Check if bus is from day before
-    int tipo_dia_int = atoi(tipo_dia);
-    if (strcmp(dia_anterior, "S") == 0) {
-        tipo_dia_int = (tipo_dia_int == 1) ? 3 : tipo_dia_int - 1;
-    }
-
-    VFT* vft = create_vft();
-    vft->tipo_dia = tipo_dia_int;
-    vft->cod_variante = strdup(variante);
-    vft->frecuencia = strdup(frecuencia);
-    vft->cod_ubic_parada = strdup(cod_ubic_parada);
-    vft->ordinal = strdup(ordinal);
-    vft->hora = strdup(hora);
-    vft->dia_anterior = strdup(dia_anterior);
-    vft->X = NULL;
-    vft->Y = NULL;
-
-    return vft;
-}
-
-VFD* row_to_vfd(char* line) {
+char* create_vfd_key(char* line) {
     char* id = strtok(line, ",");
     char* codigoEmpresa = strtok(NULL, ",");
     char* frecuencia = strtok(NULL, ",");
@@ -159,9 +121,19 @@ VFD* row_to_vfd(char* line) {
     char* longitud = strtok(NULL, ",");
     char* fecha = strtok(NULL, ",");
 
+    UNUSED(id);
+    UNUSED(codigoEmpresa);
+    UNUSED(codigoBus);
+    UNUSED(linea);
+    UNUSED(sublinea);
+    UNUSED(tipoLinea);
     UNUSED(tipoLineaDesc);
+    UNUSED(destino);
     UNUSED(destinoDesc);
+    UNUSED(subsistema);
     UNUSED(subsistemaDesc);
+    UNUSED(version);
+    UNUSED(velocidad);
 
     // Check if latitud or longitud is 0
     if (
@@ -172,56 +144,54 @@ VFD* row_to_vfd(char* line) {
         return NULL; // Skip this row
     }
 
-    VFD* vfd = create_vfd();
-    vfd->id = strdup(id);
-    vfd->codigoEmpresa = strdup(codigoEmpresa);
-    vfd->frecuencia = strdup(frecuencia);
-    vfd->codigoBus = strdup(codigoBus);
-    vfd->variante = strdup(variante);
-    vfd->linea = strdup(linea);
-    vfd->sublinea = strdup(sublinea);
-    vfd->tipoLinea = strdup(tipoLinea);
-    vfd->destino = strdup(destino);
-    vfd->subsistema = strdup(subsistema);
-    vfd->version = strdup(version);
-    vfd->velocidad = strdup(velocidad);
-    vfd->latitud = strdup(latitud);
-    vfd->longitud = strdup(longitud);
-    vfd->fecha = strdup(fecha);
-
-    return vfd;
-}
-
-char* create_vfd_key(char* line) {
-    VFD* vfd = row_to_vfd(line);
-    if (vfd == NULL) return NULL;
-
     // Extract the date (yyyy-mm-dd) from the date string
     char date[11]; // yyyy-mm-dd is 10 characters + null terminator    
-    ajustar_fecha(vfd->fecha, vfd->frecuencia, date);
+    ajustar_fecha(fecha, frecuencia, date);
 
     // Create the group key "<variante>_<frecuencia>_<dia>\0"
-    size_t key_length = strlen(vfd->variante) + strlen(vfd->frecuencia) + strlen(date) + 2 + 1; 
+    size_t key_length = strlen(variante) + strlen(frecuencia) + strlen(date) + 2 + 1; 
     char* key = (char*)malloc(key_length * sizeof(char));
 
     // Create the key in the format variante_frecuencia_yyyy-mm-dd
-    snprintf(key, key_length, "%s_%s_%s", vfd->variante, vfd->frecuencia, date);
+    snprintf(key, key_length, "%s_%s_%s", variante, frecuencia, date);
 
-    free_vfd(vfd);
     return key;
 }
 
 char* create_vft_key(char* line) {
-    VFT* vft = row_to_vft(line);
-    if (vft == NULL) return NULL;
+    char* tipo_dia = strtok(line, ";");
+    char* variante = strtok(NULL, ";");
+    char* frecuencia = strtok(NULL, ";");
+    char* cod_ubic_parada = strtok(NULL, ";");
+    char* ordinal = strtok(NULL, ";");
+    char* hora = strtok(NULL, ";");
+    char* dia_anterior = strtok(NULL, ";");
+    char* latitud = strtok(NULL, ";");
+    char* longitud = strtok(NULL, ";");
 
-    size_t key_length = strlen(vft->cod_variante) + strlen(vft->frecuencia) + 3 + 1; // "<cod_variante>_<frecuencia>_<tipo_dia>\0"
+    UNUSED(cod_ubic_parada);
+    UNUSED(ordinal);
+    UNUSED(hora);
+    UNUSED(latitud);
+    UNUSED(longitud);
+
+    if (tipo_dia == NULL || variante == NULL || frecuencia == NULL || dia_anterior == NULL) {
+        fprintf(stderr, "Error: Missing data from row to VFT in line: %s", line);
+        return NULL;
+    }
+
+    // Check if bus is from day before
+    int tipo_dia_int = atoi(tipo_dia);
+    if (strcmp(dia_anterior, "S") == 0) {
+        tipo_dia_int = (tipo_dia_int == 1) ? 3 : tipo_dia_int - 1;
+    }
+
+    size_t key_length = strlen(variante) + strlen(frecuencia) + 3 + 1; // "<cod_variante>_<frecuencia>_<tipo_dia>\0"
     char* key = (char*)malloc(key_length * sizeof(char));
 
-    // Create the vft key
-    snprintf(key, key_length, "%s_%s_%d", vft->cod_variante, vft->frecuencia, vft->tipo_dia);
+    // Create the vft keydate
+    snprintf(key, key_length, "%s_%s_%d", variante, frecuencia, tipo_dia_int);
 
-    free_vft(vft);
     return key;
 }
 
@@ -249,6 +219,7 @@ HashMap* group_data_by_vft(char** assigned_files) {
         if ((read = getline(&line, &len, file)) != -1) {
             // Optionally, print or process the header line
             printf("Header: %s", line);
+            // add_vft_to_map(map, "0", line);
         }
 
         // Ensure line is freed before the next read
@@ -270,8 +241,7 @@ HashMap* group_data_by_vft(char** assigned_files) {
 
             // Add the row to the corresponding group
             if (key != NULL) {
-                VFT* vft = row_to_vft(line);
-                add_vft_to_map(map, key, vft);
+                add_vft_to_map(map, key, line);
                 free(key);
             } else {
                 perror("Invalid Data");
@@ -281,7 +251,7 @@ HashMap* group_data_by_vft(char** assigned_files) {
             free(line_copy); // Free the copy after processing
         }
 
-        free(line);
+        // free(line);
         fclose(file);
     }
 
@@ -319,6 +289,10 @@ HashMap* group_data_by_vfd(char** assigned_files, HashMap* vft_map) {
     // Create a hash map for vfd_map
     HashMap *vfd_map = create_hash_map();
 
+    // Insert header line from VFT map to VFD map.
+    // Entry* vft_h = hash_map_search(vft_map, "0");
+    // if (vft_h != NULL && vft_h->vft_row_count > 0) add_vfd_to_map(vfd_map, "0", vft_h->vft_rows[0]);
+
     HashMap *discarded_vfds = create_hash_map();
 
     for (int i = 0; assigned_files[i] != NULL; i++) {
@@ -336,6 +310,7 @@ HashMap* group_data_by_vfd(char** assigned_files, HashMap* vft_map) {
         if ((read = getline(&line, &len, file)) != -1) {
             // Optionally, print or process the header line
             printf("Header: %s", line);
+            // add_vfd_to_map(vfd_map, "0", line);
         }
 
         // Ensure line is freed before the next read
@@ -363,8 +338,7 @@ HashMap* group_data_by_vfd(char** assigned_files, HashMap* vft_map) {
                 if (vfd_entry != NULL) {
                     printf("ENCONTRO VFD EN MAP!\n");
                     // Add the row to the vfd_map
-                    VFD* vfd = row_to_vfd(line);
-                    vfd_entry = insert_to_vfds(vfd_entry, vfd);
+                    insert_to_vfds(vfd_entry, line);
                 } else if (!hash_map_search(discarded_vfds, vfd_key)) {
                     // Generate VFT and look for data in the vft_map
                     char* vft_key = create_vft_from_vfd(vfd_key);
@@ -373,8 +347,7 @@ HashMap* group_data_by_vfd(char** assigned_files, HashMap* vft_map) {
 
                     if (vft_entry != NULL) {
                         printf("ENCONTRO VFT EN MAP!\n");
-                        VFD* vfd = row_to_vfd(line);
-                        Entry* vfd_entry = add_vfd_to_map(vfd_map, vfd_key, vfd);
+                        Entry* vfd_entry = add_vfd_to_map(vfd_map, vfd_key, line);
                         repoint_vfts_to_vfd_map(vfd_entry, vft_entry);
                     } else {
                         printf("VFT %s does not exist.\n", vft_key);
@@ -393,7 +366,7 @@ HashMap* group_data_by_vfd(char** assigned_files, HashMap* vft_map) {
             free(line_copy); // Free the copy after processing
         }
 
-        free(line);
+        // free(line);
         fclose(file);
     }
 
